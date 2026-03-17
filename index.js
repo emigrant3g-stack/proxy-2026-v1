a17bb163214b0080801d869cd433729c3b1c35f8
emigrant3g-stack <emigrant3g@gmail.com>
2026-03-17 21:42:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Проксируем все запросы с вашего адреса на API Telegram
+app.use('/', createProxyMiddleware({
+    target: 'https://api.telegram.org',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/': '/', // сохраняем путь запроса
+    },
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Proxy bridge is running on port ${PORT}`);
+});
